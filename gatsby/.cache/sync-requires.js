df02b5eaca354fa4323337561ed9efa78627c3b0
Tim Kelly <tim.kelly@sca.com.au>
2021-02-10 16:58:01 +1100

    Created a simple navigation

--- gatsby/.cache/sync-requires.js
+++ gatsby/.cache/sync-requires.js
@@ -7,10 +7,10 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/tim.kelly/Documents/Other/master-gatsby/gatsby/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/tim.kelly/Documents/Other/master-gatsby/gatsby/src/pages/404.js"))),
-  "component---src-pages-beer-js": hot(preferDefault(require("/Users/tim.kelly/Documents/Other/master-gatsby/gatsby/src/pages/beer.js"))),
+  "component---src-pages-beers-js": hot(preferDefault(require("/Users/tim.kelly/Documents/Other/master-gatsby/gatsby/src/pages/beers.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/tim.kelly/Documents/Other/master-gatsby/gatsby/src/pages/index.js"))),
   "component---src-pages-order-js": hot(preferDefault(require("/Users/tim.kelly/Documents/Other/master-gatsby/gatsby/src/pages/order.js"))),
   "component---src-pages-pizza-js": hot(preferDefault(require("/Users/tim.kelly/Documents/Other/master-gatsby/gatsby/src/pages/pizza.js"))),
-  "component---src-pages-slicemaster-js": hot(preferDefault(require("/Users/tim.kelly/Documents/Other/master-gatsby/gatsby/src/pages/slicemaster.js")))
+  "component---src-pages-slicemasters-js": hot(preferDefault(require("/Users/tim.kelly/Documents/Other/master-gatsby/gatsby/src/pages/slicemasters.js")))
 }
 
--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function SlicemasterPage() {
   return (
     <div>
+      <Nav />
       <p>Hey! I'm the slicemaster page</p>
     </div>
   );
